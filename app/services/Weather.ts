540213cbe96a08e88171515658320a4ed656ba01
David Rivera Morales <driveramorales46@gmail.com>
2023-09-18 23:10:21 -0600

    added func for getting weather by coords

--- app/services/Weather.ts
+++ app/services/Weather.ts
@@ -1,9 +1,28 @@
 class Weather {
+  API_KEY = "";
   async getWeather(city: string) {
-    const API_KEY = "";
     try {
       const response = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.API_KEY}`
+      );
+
+      if (!response.ok) {
+        throw new Error(`Failed to fetch weather data: ${response.statusText}`);
+      }
+
+      const data = await response.json();
+      console.log(data);
+      return data;
+    } catch (error) {
+      console.error("Error fetching weather data:", error);
+      throw error;
+    }
+  }
+
+  async getWeatherByCoords(lat: number, lon: number) {
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${this.API_KEY}`
       );
 
       if (!response.ok) {
