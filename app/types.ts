d274b77c902083dfd6e59f56894568460d83fe9a
RPTED255 <124104634+RPTED255@users.noreply.github.com>
2023-09-23 10:54:55 -0600

    Actualización de tipos
    Añadidos tipos más generales para los tipos de prueba a realizar

--- app/types.ts
+++ app/types.ts
@@ -1,12 +1,23 @@
-type WeatherData = {
-  departure: {
-    temperature: number | undefined;
-    status: string | undefined;
-  };
-  arrival: {
-    temperature: number | undefined;
-    status: string | undefined;
-  };
+type Coordinates = {
+	lat: number;
+	lon: number;
 };
 
-export type { WeatherData };
+type Ticket = {
+	num_ticket: string;
+	origin: string;
+	destination: string;
+	origin_latitude: number;
+	origin_longitude: number;
+	destination_latitude: number;
+	destination_longitude: number;
+};
+
+type BoletoDecoded = {
+	departureCoords: Coordinates;
+	arrivalCoords: Coordinates;
+	departureCity: string;
+	arrivalCity: string;
+};
+
+export type { Coordinates, Ticket, BoletoDecoded };
